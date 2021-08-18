e1bc3f227f1b97108d04cc0a3bf69524ff9cf05d
Nikita_Missile <carolinafernadada@gmail.com>
2021-08-18 14:26:19 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d"); // vai passar a tratar o arquivo como um plano 2d
 let box = 32; // cada quadradinho tem 32px
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function CriarBG() {
     context.fillStyle = "lightgreen"; //cor do fundo
     context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retangulo onde o jogo vai acontecer
 
 }
+
+function criarCobrinha() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+CriarBG();
+criarCobrinha();
